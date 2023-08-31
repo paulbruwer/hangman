e22936d8c6d7c729218d9f610852e8d4e7a5fcea
Paul Bruwer <paulbruwer56@gmail.com>
2023-08-31 14:00:41 +0200

    add comments to wordbar.js

--- src/components/WordBar.js
+++ src/components/WordBar.js
@@ -2,8 +2,11 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 function WordBar(props) {
+  // the state the tracks how far the user has guess the word for the store
   const wordObj = useSelector((state) => state.word.userProgress);
 
+  // when the userProgress state is updated the this function loops through the object, saving
+  // it to a variable which is returned by the function
   const handelWordBar = () => {
     let line = "";
     for (const key in wordObj) {
@@ -12,6 +15,7 @@ function WordBar(props) {
     return line;
   };
 
+  // the return from out function is posted to a paragraph element on the landing page
   return (
     <div className={props.isVisible}>
       <p style={{ fontSize: "5vw", fontFamily: "sans-serif" }}>
